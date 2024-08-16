6706249720cddac4908e43d236aa5d2ecab53486
Suprith.S <Suprith.S@ust.com>
2024-08-16 22:32:51 +0530

    added login feature with its steps and pom

---
+++ src/test/java/com/automation/steps/HomeSteps.java
@@ -0,0 +1,43 @@
+package com.automation.steps;
+
+import com.automation.pages.HomePage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class HomeSteps {
+
+    HomePage homePage = new HomePage();
+
+    @Given("user is on website")
+    public void user_is_on_website() {
+
+    }
+
+    @When("user select his desired city")
+    public void user_select_his_desired_city() {
+
+    }
+
+    @Then("verify user is on the home page")
+    public void verify_user_is_on_the_home_page() {
+
+    }
+
+    @Then("user can view city-specific services")
+    public void user_can_view_city_specific_services() {
+
+    }
+
+    @When("user click on login button")
+    public void user_click_on_login_button() {
+
+    }
+
+    @Then("verify login is successful")
+    public void verify_login_is_successful() {
+
+    }
+
+
+}
---
+++ src/test/java/com/automation/steps/LoginSteps.java
@@ -0,0 +1,38 @@
+package com.automation.steps;
+
+import com.automation.pages.LoginPage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+    LoginPage loginPage = new LoginPage();
+
+    @Then("verify user is on the login page")
+    public void verify_user_is_on_the_login_page() {
+
+    }
+
+    @When("user enter valid phone number {string} and password {string}")
+    public void user_enter_valid_phone_number_and_password(String phoneNo, String password) {
+
+    }
+
+    @When("click on I'm not a robot checkbox")
+    public void click_on_i_m_not_a_robot_checkbox() {
+
+    }
+
+    @When("click on login with Password button")
+    public void click_on_login_with_password_button() {
+
+    }
+
+    @When("user enter invalid phone number {string} and password {string}")
+    public void userEnterInvalidPhoneNumberAndPassword(String phoneNumber, String password) {
+    }
+
+    @Then("verify error message is displayed {string}")
+    public void verifyErrorMessageIsDisplayed(String errorMessage) {
+    }
+}
