b750511862fe4430e204433f9d506e690c8a75d3
Suprith.S <Suprith.S@ust.com>
2024-08-16 17:02:19 +0530

    updated driver manager, base page

--- src/test/java/com/automation/pages/BasePage.java
+++ src/test/java/com/automation/pages/BasePage.java
@@ -1,4 +1,14 @@
 package com.automation.pages;
 
-public class BasePage {
+import com.automation.utils.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+abstract class BasePage {
+    WebDriver driver;
+
+    public BasePage() {
+        driver = DriverManager.getDriver();
+        PageFactory.initElements(driver, this);
+    }
 }
