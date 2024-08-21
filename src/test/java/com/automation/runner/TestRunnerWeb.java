bb4d852d89d0a831ae977b8ede82ade73e00a40a
ManojKumar.HN <ManojKumar.HN@ust.com>
2024-08-21 12:04:57 +0530

    Android driver added

--- src/test/java/com/automation/runner/TestRunnerWeb.java
+++ src/test/java/com/automation/runner/TestRunnerWeb.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features/web",
-        glue = "com.automation.steps.web",
-        plugin = "json:target/web/cucumber.json"
+        glue = "com.automation.steps",
+        plugin = "json:target/Web/cucumber.json"
 )
 public class TestRunnerWeb {
 
