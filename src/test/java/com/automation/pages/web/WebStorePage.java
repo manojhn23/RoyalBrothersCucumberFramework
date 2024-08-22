69ab8868381df50231e755c4ee8018b3e1b722b5
ManojKumar.HN <ManojKumar.HN@ust.com>
2024-08-22 12:07:51 +0530

    modified

--- src/test/java/com/automation/pages/web/WebStorePage.java
+++ src/test/java/com/automation/pages/web/WebStorePage.java
@@ -1,21 +1,24 @@
 package com.automation.pages.web;
 
 
+import com.automation.pages.ui.StorePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class WebStorePage extends WebBasePage {
+public class WebStorePage extends WebBasePage implements StorePage {
 
     @FindBy(css = ".list-menu.list-menu--inline")
     WebElement menuList;
 
     String categoryPath = "//ul[@class='list-menu list-menu--inline']//span[text()='%s']";
 
+    @Override
     public boolean isUserOnStorePage() {
         return menuList.isDisplayed();
     }
 
+    @Override
     public void selectProductCategory(String productCategory) {
         WebElement category = driver.findElement(By.xpath(String.format(categoryPath, productCategory)));
         category.click();
